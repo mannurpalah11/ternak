94932d710f769ccae25f900b9acc6506b1b3b94b
mannurpalah11 <102415261+mannurpalah11@users.noreply.github.com>
2023-08-07 17:37:59 +0700

    Add files via upload

---
+++ data/BalaiDesa_5.js
@@ -0,0 +1,1 @@
+var json_BalaiDesa_5 = {"type":"FeatureCollection","name":"BalaiDesa_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"OBJECTID":4.0,"id":4.0,"x":109.82338,"y":-7.25914,"ket":"Balai Desa Pegundungan"},"geometry":{"type":"Point","coordinates":[109.82337600000011,-7.259135]}}]}
---
+++ data/PeternakanSariTani_4.js
@@ -0,0 +1,1 @@
+var json_PeternakanSariTani_4 = {"type":"FeatureCollection","name":"PeternakanSariTani_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","ket":"Peternakan Sari Tani"},"geometry":{"type":"Point","coordinates":[109.819952029522,-7.255493424131874]}}]}
